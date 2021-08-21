8fdb80174b200794f5e917edbf934e0eb07ca1fe
lvzhen <15549806382@qq.com>
2021-08-21 22:27:36 +0800

    删掉多余接口

--- app.py
+++ app.py
@@ -21,50 +21,6 @@ init_upload_dir()
 def index():
     return render_template('index.html')
 
-@app.route("/users", methods=['GET', 'POST'])
-def users():
-    if request.method == 'POST':
-      print(request.form)
-      uid = request.form.get('uid');
-      user = {'uid': uid, 'name': 'test1'}
-      return jsonify(data=user)
-    else:
-      limit = request.args.get('limit')
-      data = {
-        'count': limit or 2,
-        'users': [{'name': 'test1'}, {'name': 'test2'}]
-      }
-      return jsonify(data=data)
-
-@app.route("/users/<id>")
-def get_user(id):
-    return jsonify(data={'name': 'test1'})
-
-# 上传文件示例
-@app.route('/upload',methods=['POST'])
-def upload():
-  if request.method == 'POST':
-    if 'avatar' not in request.files:
-      res = {"error": "No avatar file upload"}
-      return jsonify(data=res)
-    avatar = request.files['avatar']
-
-    if avatar.filename == '':
-      res = {"error": "No avatar file selected"}
-      return jsonify(data=res)
-
-    if avatar:
-      filename = secure_filename(avatar.filename);
-      filePath = os.path.join(app.config['UPLOAD_DIR'], filename)
-      avatar.save(filePath)
-      uploadUrl = url_for('uploaded_file', filename=filename)
-      res = {'upload': uploadUrl}
-      return jsonify(data=res)
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-  return send_from_directory(app.config['UPLOAD_DIR'], filename)
-
 @app.route('/mp4', methods=['GET', 'POST'])
 def save_mp4():
   url = request.form.get('url')
